156d7f9386b46c9073bb7fe23ccb5fe37f43221f
chaodeng <645111208@qq.com>
2018-11-06 15:14:28 +0800

    update

--- springboot-mybatis/src/main/java/org/spring/springboot/controller/CityRestController.java
+++ springboot-mybatis/src/main/java/org/spring/springboot/controller/CityRestController.java
@@ -3,7 +3,6 @@ package org.spring.springboot.controller;
 import org.spring.springboot.domain.City;
 import org.spring.springboot.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +18,7 @@ public class CityRestController {
     private CityService cityService;
 
     @RequestMapping(value = "/api/city", method = RequestMethod.GET)
-    public City findOneCity(@RequestParam(value = "cityName", required = true) String cityName) {
+    public City findOneCity(@RequestParam(value = "cityName", required = false,defaultValue = "city_name") String cityName) {
         return cityService.findCityByName(cityName);
     }
 
